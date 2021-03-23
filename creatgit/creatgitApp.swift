822e1d73d7e259ea42a1f83a3964229fa82ac944
thanhduy <thanhduy2198@gmail.com>
2021-03-23 20:29:29 +0700

    Initial Commit

---
+++ creatgit/creatgitApp.swift
@@ -0,0 +1,17 @@
+//
+//  creatgitApp.swift
+//  creatgit
+//
+//  Created by Thanh Duy on 23/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct creatgitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
